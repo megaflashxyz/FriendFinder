309ac1e2fe72c49f360f069bad418e8fbb6280d7
John Gorman <johngorm@usc.edu>
2017-04-30 17:18:44 -0500

    Remove debug console.log

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,9 +6,7 @@ const _ = require('lodash');
 function calculateCompatibility(userArray, friendArray){
 	var difference = 0;
 	for(let answer in userArray){
-		console.log('User:' + userArray[answer], 'Friend' + friendArray[answer]);
 		difference += (Math.abs(userArray[answer] - friendArray[answer]));;
-		console.log(difference);
 	}
 	return difference;
 }
@@ -26,10 +24,9 @@ router.post('/api/friends', function(req, res){
 	for(var score in user_scores){
 		user_scores[score] = parseInt(user_scores[score], 10);
 	}
-	console.log('User score' , user_scores)
+	
 	//Loop through friends and calculate the difference in score from user on each ?
 	for (var person in friends){
-		console.log("Friend: " + friends[person].name, "Answers " + friends[person].scores)
 		let difference = calculateCompatibility(user_scores, friends[person].scores)
 		if(difference < differential_score){
 			differential_score = difference;
@@ -37,7 +34,6 @@ router.post('/api/friends', function(req, res){
 			best_match_data.photo = friends[person].photo;
 		}
 	}
-	console.log(best_match_data);
 	res.json(best_match_data);
 })
 
